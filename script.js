f97efa3d0ac6f96b7b59dbd1425adcf1842bc271
grgskrgnns <37068150+grgskrgnns@users.noreply.github.com>
2021-04-02 16:09:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+	
+	if ((p === '') || (p <= 0)) {
+      alert('Please enter a positive number');
+	  document.getElementById("principal").focus();
+	} else {
+
+		r = document.getElementById("rate").value;
+		
+		y = document.getElementById("years").value;
+		
+		res = p*r*y/100.0;
+		
+		n = Number(new Date().getFullYear()) + Number(y);
+		
+		
+		
+		document.getElementById('mypara').innerHTML = 'if you deposit <mark>' +p+ '</mark></br>at an interest rate of <mark>'+r
+		+' %</mark></br>You will receive an amount of <mark>'+res+'</mark>,</br>in the year <mark>'+n+'</mark>';
+	}
     
 }
         
